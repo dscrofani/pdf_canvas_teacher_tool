8d4084c33d23070c87182374c97eac8a7e24828e
David Scrofani <david.scrofani@gmail.com>
2018-05-10 17:47:39 -0400

    I added the timesync node module

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 
 
 app.use(express.static(__dirname + '/public')); //That's a double underscore
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.listen(port, function() {
-  console.log('Great! The server is running and waiting for traffic on port 3000.')
+  console.log('Great! The server is running and waiting for traffic on port 5000.')
 });
 
 //I am done
