5c489d249c2cf40edd7cf4c9645b82f8dac3f385
David Scrofani <david.scrofani@gmail.com>
2018-05-11 07:50:10 -0400

    I finally fixed the port problem for real yo

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
-var timesyncServer = require('timesync/server');
 
 app.use(express.static(__dirname + '/public')); //That's a double underscore
 
-app.get('/', function (req, res) {
-  res.sendFile('index.html');
-});
+// app.get('/', function (req, res) {
+//   res.sendFile('index.html');
+// });
 
 app.listen(port, function() {
   console.log('Great! The server is running and waiting for traffic on port 5000.')
 });
-
-app.use('/timesync', timesyncServer.requestHandler);
